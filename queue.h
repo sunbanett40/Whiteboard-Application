9e59ed1702713748ebd42ea49928736fb6a82339
Joseph Butterworth <josephdavidbutterworth@gmail.com>
2021-04-24 15:14:06 +0100

    queue header

--- queue.h
+++ queue.h
@@ -1,11 +1,24 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include <QQueue>
 
+template <typename  T>
 class queue
 {
 public:
-    queue();
+    explicit queue(int maxSize);
+
+    void pushToQueue(T const &pushedItem);
+    void pullFromQueue(T &pushedItem);
+
+private:
+    int currentSize;
+    int maxSize;
+
+    //queue of data packets
+    QQueue<T> serialisedQueue;
+
 };
 
 #endif // QUEUE_H
